704ba8bbe4bd7502e036ecb535d9bc97873aabc9
CIANDT\isabelladutra <t-isabella.salgueiro@bradescoseguros.com.br>
2022-02-07 10:12:47 -0300

    Primeiro commit

---
+++ src/test/java/com/project/ibp/louvoribp/LouvorIbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ibp.louvoribp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LouvorIbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
